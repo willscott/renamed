c53eb68157b4e09c3d5f2adf009385d072ef7338
Will Scott <willscott@gmail.com>
2015-12-10 08:28:03 -0800

    initial implementation

---
+++ index.js
@@ -0,0 +1,118 @@
+var dns = require('native-dns');
+var checksum = require('crc-32');
+
+if (process.argv.length < 3) {
+  console.log('Usage: renamed <domain name> <public IP of server>');
+  process.exit();
+}
+var rootTLD = process.argv[1].toLowerCase();
+var myip = process.argv[2];
+var server = dns.createServer();
+var sessionDigestKey = require('uuid').v4();
+
+var createChecksum = function (client, server) {
+  var str = client + '-' + server + '-' + sessionDigestKey;
+  str = str.replace('.', 'a').replace(':', 'b')
+  return checksum.str(str);
+}
+
+// Return [client,server] if query is valid, or false if not.
+var validateQuery = function (query) {
+  query = query.replace('a', '.').replace('b', ':');
+  var parts = query.split('-');
+  if (parts.length != 3) {
+    return false;
+  }
+  var client = parts[0];
+  var server = parts[1];
+  var expectedChecksum = createChecksum(client, server);
+  if (parts[2] === expectedChecksum) {
+    return [client, server];
+  } else {
+    return false;
+  }
+}
+
+var matchServer = function (client) {
+  var server = '8.8.8.8'; //todo: roundrobin.
+  return server;
+};
+
+var createPrefix = function (client, server) {
+  var checksum = createChecksum(client, server);
+  var prefix = client + '-' + server + '-' + checksum;
+  return prefix.replace('.', 'a').replace(':', 'b');
+};
+
+server.on('request', function (req, resp) {
+  if (req.question.length > 0 && req.question[0].type == 'A') {
+    var query = req.question[0].name.toLowerCase();
+    console.log(req.address + ':' + query);
+    // Failures
+    if (query.indexOf(rootTLD) === -1) {
+      resp.answer.push(dns.A({
+        name: query,
+        address: myip,
+        ttl: 5
+      }));
+      resp.send();
+      return;
+    // Initial query
+    } else if (query === rootTLD) {
+      // TODO: record attempt at connectivity
+      var server = matchServer(req.address);
+      var delegee = createPrefix(req.address, server) + '.' + rootTLD;
+      resp.answer.push(dns.CNAME({
+        name: query,
+        ttl: 5,
+        data: delegee
+      }));
+      resp.additional.push(dns.NS({
+        name: delegee,
+        ttl: 5,
+        data: 'ns1.' + delegee
+      }));
+      resp.additional.push(dns.A({
+        name: 'ns1.' + delgee,
+        address: server,
+        ttl: 5
+      }));
+      resp.send();
+      return;
+    }
+    // Successful indicaton of connectivity
+    var prefix = query.split(rootTLD)[0];
+    prefix = prefix.substr(0, prefix.length - 1);
+    var parts = validateQuery(prefix);
+    if (parts === false) {
+      resp.answer.push(dns.A({
+        name: query,
+        address: myip,
+        ttl: 5
+      }));
+      resp.send();
+      return;
+    } else {
+      // TODO: Log success of client-server. query.
+      // TODO: See if it's been made recently by same server, indicating response dropped.
+      resp.answer.push(dns.CNAME({
+        name: query,
+        ttl: 5,
+        data: 'www.google.com'
+      }));
+      resp.answer.push(dns.A({
+        name: 'www.google.com',
+        address: '173.194.123.52',
+        ttl: 5
+      }));
+      resp.send();
+      return;
+    }
+  }
+});
+
+server.on('error', function (err) {
+  console.warn(err.stack);
+});
+
+server.serve(53);
